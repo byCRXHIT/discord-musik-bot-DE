d8772c9121c4ebe885360fe2ed52461a3debbbcc
Jaro <70042525+byCRXHIT@users.noreply.github.com>
2021-02-23 15:17:29 +0100

    fixed time out

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ bot.musicStartCommand({
     $description[Spiele nun: **$songInfo[title]**
 Länge: **$songInfo[duration] Minuten**
 Hochgeladen von: **$songInfo[publisher]**]
+$footer[Gemacht von byCRXHIT]
     $color[GREEN]
     $thumbnail[$songInfo[thumbnail]]
     $clear[1]`
@@ -22,7 +23,8 @@ bot.onMessage()
 bot.command({
     name: "play",
 	aliases: ["p"],
-    code: `$playSong[$message;:x: Songe konnte nicht abgespielt werden!]`
+    code: `$playSong[$message;:x: Songe konnte nicht abgespielt werden!]
+	$argsCheck[>1;Benutze: !play <song name>]`
 })
 
 bot.command({
@@ -37,6 +39,14 @@ bot.command({
     $description[Song wurde gestoppt: $songInfo[title]]
     $color[RED]
     $stopSong`
+})
+
+bot.command({
+	name: "volume",
+	aliases: ["v"],
+	code: `Die lautstärke wurde zu $message[1] gestellt!
+	$argsCheck[1;Benutze: !volume <nummer>]
+	$volume[$message]`
 })
 
 bot.command({
