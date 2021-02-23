8cc2d8f93e376412eeba01ad792cd41ecce8190f
Jaro <70042525+byCRXHIT@users.noreply.github.com>
2021-02-23 14:57:11 +0100

    Fixed time out

--- server.js
+++ server.js
@@ -2,59 +2,44 @@ const dbd = require("dbd.js")
 const config = require("./config.json")
 
 const bot = new dbd.Bot({
-token: config.token,
-prefix: "!" 
+	token: config.token,
+	prefix: "!"
 })
 
-bot.status({
-      text: "gemacht von: byCRXHTI",
-      type: "PLAYING"
+bot.musicStartCommand({
+    channel: "$channelID",
+    code: `$title[Spiele Song]
+    $description[Spiele nun: **$songInfo[title]**
+Länge: **$songInfo[duration] Minuten**
+Hochgeladen von: **$songInfo[publisher]**]
+    $color[GREEN]
+    $thumbnail[$songInfo[thumbnail]]
+    $clear[1]`
 })
-
-/*
-	Dieser Bot wurde gemacht von: byCRXHIT
-	Hier benutze ich  das "DBD.JS" npm package
-	füg deinen token in der config.json ein.
-	Credits müssen nicht angegeben werden währe aber net (^^)
-*/
 
 bot.onMessage()
 
 bot.command({
-        name: "play",
-    	aliases: ["p"],
-        code: `
-$color[5AC0DE]
-$author[Song;https://images.squarespace-cdn.com/content/v1/58eac4d88419c2d993e74f57/1491954929572-MII6RYJO4QRPZHRFNYAM/ke17ZwdGBToddI8pDm48kOyctPanBqSdf7WQMpY1FsRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpyD4IQ_uEhoqbBUjTJFcqKvko9JlUzuVmtjr1UPhOA5qkTLSJODyitRxw8OQt1oetw/ITunes_12_logo.png]
-$description[Ich spiele gerade: **$songInfo[title]**, die länge ist: **$songInfo[duration]** seconds]
-$footer[Position in Warteschlange: $queueLength]
-$playSong[$message;Ein Fehler ist aufgetreteten. Bitte versuche es später nocheinmal]
-$onlyIf[$message!=;Schreib einen song dahinter!]
-$onlyIf[$voiceID!=;Du bist nicht in einem Sprach-Channel!]`
+    name: "play",
+	aliases: ["p"],
+    code: `$playSong[$message;:x: Songe konnte nicht abgespielt werden!]`
 })
 
 bot.command({
-        name: "queue",
-    	aliases: ["q"],
-        code: `
-$color[5AC0DE]
-$author[Song zur Warteschlange hinzugefügt!;https://images.squarespace-cdn.com/content/v1/58eac4d88419c2d993e74f57/1491954929572-MII6RYJO4QRPZHRFNYAM/ke17ZwdGBToddI8pDm48kOyctPanBqSdf7WQMpY1FsRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpyD4IQ_uEhoqbBUjTJFcqKvko9JlUzuVmtjr1UPhOA5qkTLSJODyitRxw8OQt1oetw/ITunes_12_logo.png]
-$description[Song: **$message**
-Ich spiele gerade: **$songInfo[title]**]
-$footer[Position in der Warteschlange: $queueLength]
-$playSong[$message;Ein Fehler ist aufgetreteten. Bitte versuche es später nocheinmal]
-$onlyIf[$message!=;Schreib einen song dahinter!]
-$onlyIf[$voiceID!=;Du bist nicht in einem Sprach-Channel!]`
+    name:"skip",
+	aliases: ["s", "sk"],
+    code:`$skipSong`
 })
 
 bot.command({
-        name: "volume",
-    	aliases: ["v"],
-        code: `
-$color[5AC0DE]
-$author[Volume Changed!;https://images.squarespace-cdn.com/content/v1/58eac4d88419c2d993e74f57/1491954929572-MII6RYJO4QRPZHRFNYAM/ke17ZwdGBToddI8pDm48kOyctPanBqSdf7WQMpY1FsRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpyD4IQ_uEhoqbBUjTJFcqKvko9JlUzuVmtjr1UPhOA5qkTLSJODyitRxw8OQt1oetw/ITunes_12_logo.png]
-$description[The volume got changed to: $message dezibel]
-$volume[$message[1]]
-$onlyIf[$message!=;Bitte schreib eine nummer dahinter zwischen 100 - 300!]
-$onlyIf[$voiceID!=;Du bist nicht in einem Sprach-Channel]`
+    name: "stop",
+    code: `$title[Stoppe Song!]
+    $description[Song wurde gestoppt: $songInfo[title]]
+    $color[RED]
+    $stopSong`
+})
+
+bot.command({
+    name: "ping",
+    code: `$ping ms`
 })
